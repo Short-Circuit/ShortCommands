eae7ae9242f08e0047530eda1b84faa62fafe889
Short-Circuit <P_Ir_At_Es@comcast.net>
2014-11-12 17:20:32 -0800

    Initial commit

--- src/com/shortcircuit/shortcommands/exceptions/ConsoleOnlyException.java
+++
@@ -1,9 +0,0 @@
-package com.shortcircuit.shortcommands.exceptions;
-
-/**
- * @author ShortCircuit908
- * 
- */
-public class ConsoleOnlyException extends Exception{
-    private static final long serialVersionUID = 1803155064915494679L;
-}
--- src/com/shortcircuit/shortcommands/exceptions/TooFewArgumentsException.java
+++
@@ -1,9 +0,0 @@
-package com.shortcircuit.shortcommands.exceptions;
-
-/**
- * @author ShortCircuit908
- * 
- */
-public class TooFewArgumentsException extends Exception {
-    private static final long serialVersionUID = -4091705821015041002L;
-}
